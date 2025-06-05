69118af6dd36659b3e0ae57315b582758acca065
Zahar <gmirianskiy@gmail.com>
2025-06-05 22:26:24 +0300

    add fix

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -1,10 +1,6 @@
 package mate.academy.service;
 
 import mate.academy.model.User;
-import mate.academy.service.UserService;
-
-import static mate.academy.service.UserService.users;
-import static mate.academy.service.UserService.getUsers;
 
 public class AuthenticationService {
     
@@ -12,9 +8,11 @@ public class AuthenticationService {
         UserService userService = new UserService();
         User foundUser = userService.findByEmail(email);
         if (foundUser != null && password.equals(foundUser.getPassword())){
+
             return true;
         }
-        return  false;
+
+        return false;
     }
 
 }
