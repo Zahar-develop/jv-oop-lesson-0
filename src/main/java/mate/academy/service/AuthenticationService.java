8b48d22f88a7fe47e0f6f0b698ed683cab8b61d2
Zahar <gmirianskiy@gmail.com>
2025-06-19 22:09:08 +0300

    added comment

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -4,7 +4,14 @@ import mate.academy.model.User;
 
 public class AuthenticationService {
     private UserService userService = new UserService();
-
+    /**
+     * Authenticates a user by email and password.
+     *
+     * @param email the user's email address
+     * @param password the user's password
+     * @return true if a user with the given email exists and the password matches,
+     *         false otherwise
+     */
     public boolean login(String email, String password) {
         User foundUser = userService.findByEmail(email);
         return foundUser != null && password.equals(foundUser.getPassword());
